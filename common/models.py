e98190adadc578e71f55c1d5ddc5112d4910f34a
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-06-29 23:02:49 +0400

    Add Common & Dashboard apps, basic Contact and Source models in common

--- common/models.py
+++ common/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Source(models.Model):
+    source_type = models.CharField(default="manual", max_length=100)
+    name = models.CharField(default=_("Manually Added"), max_length=100)
+    description = models.TextField(default=_("This contact was manually added to the database."))
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True)
+    company = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+
+    last_sent = models.DateTimeField(blank=True, null=True)
+    subscribed = models.BooleanField(default=True)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=255)
