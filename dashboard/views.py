2732f67179033bf2ae1e743ae7f008649b762675
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-01 00:24:43 +0400

    Basic Navigation + Basic Index view for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+base_context = {
+    'name': "I-Trust Systems",
+}
+
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', base_context)
