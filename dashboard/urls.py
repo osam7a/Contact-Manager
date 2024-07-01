0bc3bc5948baebc4f6c2f9e98db384001ee0e6ad
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-01 14:41:01 +0400

    include contacts view in dashboard URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import index
+from .views import index, contacts
 
 urlpatterns = [
     path('', index),
+    path('contacts/', contacts, name='contacts'),
 ]
