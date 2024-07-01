b3f1dd5cba118730963d8887823386a50d8cf512
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-02 00:04:59 +0400

    Basic context management, and Pagination for contacts view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,34 @@
+from copy import deepcopy
+from datetime import timedelta
+
 from django.shortcuts import render
+from django.utils import timezone
+
+from .paginator import Paginator
+from apps.common.models import Contact
 
 base_context = {
     'name': "I-Trust Systems",
 }
 
+PAGE_SIZE = 13
+
 def index(request):
-    return render(request, 'index.html', base_context)
+    context = deepcopy(base_context)
+    context['contacts'] = Contact.objects.all()
+    context['new_contacts'] = Contact.objects.filter(created_at__gte=timezone.now()-timedelta(days=7))
+    context['new_contacts_percentage'] = len(context['new_contacts']) / len(context['contacts']) * 100
+
+    return render(request, 'index.html', context)
 
 def contacts(request):
-    return render(request, 'pages/contacts.html', base_context)
+    paginator = Paginator(PAGE_SIZE, Contact.objects.all())
+    page = request.GET.get('p', 1)
+    paginated_contacts = paginator.get_page(int(page))
+
+    context = deepcopy(base_context)
+    context['contacts'] = paginated_contacts
+    context['total_contacts'] = Contact.objects.count()
+    context['pages_count'] = paginator.get_num_pages()
+
+    return render(request, 'pages/contacts.html', context)
