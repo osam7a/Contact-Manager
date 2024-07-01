d976e987dcbf6ebfca45cc9a08912430a2568d39
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-01 14:41:11 +0400

    Contacts view

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,8 @@ base_context = {
     'name': "I-Trust Systems",
 }
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', base_context)
+
+def contacts(request):
+    return render(request, 'pages/contacts.html', base_context)
