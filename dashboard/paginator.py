164490ee59bcc0f048a69310bb5f560df6cb97f0
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-02 00:04:28 +0400

    basic Pagination class

---
+++ dashboard/paginator.py
@@ -0,0 +1,12 @@
+class Paginator:
+    def __init__(self, page_size, items):
+        self.page_size = page_size
+        self.items = items
+
+    def get_page(self, page_number):
+        start = (page_number - 1) * self.page_size
+        end = start + self.page_size
+        return self.items[start:end]
+    
+    def get_num_pages(self):
+        return len(self.items) // self.page_size + 1
