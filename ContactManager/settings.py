61ba2b09e276465deb5f689f4d5a40afa209bfa5
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-01 14:40:16 +0400

    added TEMPLATE_DIRS in settings to avoid TemplateNotFound

--- ContactManager/settings.py
+++ ContactManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,6 +70,10 @@ TEMPLATES = [
         },
     },
 ]
+
+TEMPLATE_DIRS = (
+    path.join(BASE_DIR, 'templates'),
+)
 
 WSGI_APPLICATION = 'ContactManager.wsgi.application'
 
