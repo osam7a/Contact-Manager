6c6dffa41ae20b94587f6a30d11b002cd9d1d56a
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-02 18:25:14 +0400

    Another typo in the interpreter path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 
-INTERP = os.environ['HOME'] + '/repositories/ContactManager/venv/bin/python'
+INTERP = os.environ['HOME'] + '/repositories/Contact-Manager/venv/bin/python'
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from ContactManager.wsgi import application
