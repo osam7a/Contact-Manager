5ca118c886ecb5df94394d27458e48199178f458
osam7a <osamaziadalhinawi@gmail.com>
2024-07-03 10:52:23 +0400

    Functional Select All

--- static/contacts.js
+++ static/contacts.js
@@ -1,4 +1,5 @@
 var selected = [];
+var allSelected = false;
 
 $("#select-all-current").on("change", function() {
     selected = [];
@@ -6,7 +7,7 @@ $("#select-all-current").on("change", function() {
     // add the numbers after select-one- to the selected array
     if ($(this).prop("checked")) {
         $("[id^=select-one-]").each(function() {
-            selected.push($(this).attr("id").split("-")[2]);
+            selected.push($(this).parents("tr").attr("id").split("-")[1]);
         });
     } else {
         selected = [];
@@ -18,7 +19,8 @@ $("#select-all-current").on("change", function() {
 
 $("[id^=select-one-]").on("change", function() {
     if ($(this).prop("checked")) {
-        selected.push($(this).attr("id").split("-")[2]);
+        // get the parent id and add it to the selected array
+        selected.push($(this).parents("tr").attr("id").split("-")[1])
     } else {
         for (var i = 0; i < selected.length; i++) {
             if (selected[i] == $(this).attr("id").split("-")[2]) {
@@ -35,4 +37,19 @@ $("[id^=select-one-]").on("change", function() {
 
     var currentText = $('#selectionCount').text().split(' ').slice(1, 5).join(' ');
     $('#selectionCount').html(selected.length + ' ' + currentText + ' <a href="#" id="select-all" class="text-blue-500">Select All</a>');
+});
+
+$("#select-all").on("click", function() {
+    allSelected = !allSelected;
+    
+    selected = [];
+    if (allSelected) {
+        $("[id^=select-one-]").prop("checked", true);
+    } else {
+        $("[id^=select-one-]").prop("checked", false);
+    }
+
+    var currentText = $('#selectionCount').text().split(' ').slice(1, 5).join(' ');
+    var totalContacts = $('#selectionCount').text().split(' ').slice(5, 6).join(' ');
+    $('#selectionCount').html(allSelected ? totalContacts : selected.length + ' ' + currentText + ` <a href="#" id="select-all" class="text-blue-500">${allSelected ? 'Unselect' : 'Select'} All</a>`);
 });
